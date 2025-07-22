452eabe2e51f7d31885f2184e22130942dd39550
Muhammad  Sholehhudin <msholehhudin.ms@gmail.com>
2025-07-22 17:53:04 +0700

    Separated new transaction

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,14 @@
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
 import ProtectedRoute from "@/context/ProtectedRoute";
 
 export default function DashboardLayout({ children }: any) {
   return (
     <SidebarProvider>
       <AppSidebar />
+      <Toaster />
       <main className="w-full p-4">
         <Navbar />
         {children}
