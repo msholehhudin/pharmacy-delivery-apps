7f3e1c41587e72a8bbfaffcff8e48b3dd3863e3c
Muhammad  Sholehhudin <msholehhudin.ms@gmail.com>
2025-07-25 11:23:27 +0700

    Reports Menu

---
+++ app/(dashboard)/reports/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reports = () => {
+  return (
+    <div className=" flex pt-10 pl-10 ">
+      <h1 className="text-4xl text-red-600">Coming Soon</h1>
+    </div>
+  );
+};
+
+export default Reports;
