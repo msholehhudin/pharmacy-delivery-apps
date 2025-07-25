fa6a03c955646a234a323fec620c30ed8b9ac242
Muhammad  Sholehhudin <msholehhudin.ms@gmail.com>
2025-07-25 11:30:57 +0700

    Settings Menu

---
+++ app/(dashboard)/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className=" flex pt-10 pl-10 ">
+      <h1 className="text-4xl text-red-600">Settings Menu is Coming Soon</h1>
+    </div>
+  );
+};
+
+export default Settings;
